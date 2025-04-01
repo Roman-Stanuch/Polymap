245d75a7591474f82b10e9a97a19c898675e673b
Polygonal Graphics <154336865+Roman-Stanuch@users.noreply.github.com>
2025-04-01 09:15:01 -0500

    FIxed windows build script

--- scripts/windows.bat
+++ scripts/windows.bat
@@ -1,11 +1,9 @@
 @echo off
 
 if not exist "../build/windows" mkdir "../build/windows"
-if not exist "../build/windows/lib" mkdir "../build/windows/lib"
 
 set "outdir=../build/windows"
 set "src=../src"
 set "srcfiles=%src%/main.c"
 
-gcc %srcfiles% -o %outdir%/app.exe -L ../lib -I ../include -lraylib -Wall
-xcopy ..\lib\raylib.dll %outdir%/lib /d > nul
+gcc %srcfiles% -o %outdir%/app.exe -L ../lib/windows -I ../include -lraylib -lopengl32 -lgdi32 -lwinmm -Wall
